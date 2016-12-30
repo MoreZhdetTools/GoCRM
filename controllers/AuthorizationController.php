4a43b1435892146f4ef77d05321c51ed3073717c
Pavel <pavel.frolov.1968@gmail.com>
2016-12-30 17:21:36 +0800

    Update AuthorizationController.php

--- controllers/AuthorizationController.php
+++ controllers/AuthorizationController.php
@@ -12,18 +12,18 @@ class AuthorizationController
   
   	private function authorize()
     {
- 				if($this->code === '') 
+ 		if($this->code === '') 
         {
-        		VK::authorize(); //Получаю код 
+            VK::authorize(); //Получаю код 
         } 
       	
       	$answer = VK::getAccessToken($this->code); //Получаю access_token
       
       	if(empty($answer['error'])) 
         {
-        		$_SESSION['access_token'] = $answer['access_token'];                                         //Сохраняю токен в сессии
-        		$_SESSION['user_id']      = $answer['user_id'];                                              //ID пользователя
-        		$_SESSION['end_session']  = date('d.m.Y H:i:s', strtotime($answer['expires_in']." seconds"));//Время принудительного разлогирования
+        	$_SESSION['access_token'] = $answer['access_token'];                                         //Сохраняю токен в сессии
+        	$_SESSION['user_id']      = $answer['user_id'];                                              //ID пользователя
+        	$_SESSION['end_session']  = date('d.m.Y H:i:s', strtotime($answer['expires_in']." seconds"));//Время принудительного разлогирования
          
           	$user = VK::getUser();                                                                      //Получаю информацию о пользователе
           	Users::addUser($user['uid'], $user['first_name'], $user['last_name'], date('Y-m-d H:i:s')); //Добавляю нового пользователя в БД 
